7d0dd81ff2ab1465adb027b4b9983e696342e615
Surya Kiran <tssuryakiran@gmail.com>
2026-03-10 15:19:53 +0000

    Employee sidebar updated

--- src/components/empDashboard/EmployeeSidebar.jsx
+++ src/components/empDashboard/EmployeeSidebar.jsx
@@ -31,7 +31,7 @@ const EmployeeSidebar = () => {
         <NavLink to={`/employee-dashboard/leaves/${user._id}`} 
           className={({isActive}) => `${isActive ? "bg-teal-500" : " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
           <FaBuilding />
-          <span>Leaves</span>
+          <span>Leave</span>
         </NavLink>
 
         <NavLink to={`/employee-dashboard/salary/${user._id}`} 
